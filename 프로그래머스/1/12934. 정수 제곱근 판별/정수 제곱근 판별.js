28a2cbe887fa4db8fd3bef40b0655bd465bb64bc
jsoh07 <85730620+jsoh07@users.noreply.github.com>
2024-10-09 23:38:04 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    let answer = 0;
+    let sqrt = Math.sqrt(n);
+    if(sqrt % 1 === 0) answer = (sqrt + 1) * (sqrt + 1);
+    else answer = -1;
+    
+    return answer;
+}
