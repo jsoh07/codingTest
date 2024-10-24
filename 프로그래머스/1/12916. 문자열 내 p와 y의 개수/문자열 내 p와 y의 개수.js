8567b5222d54e35f7169c9c95e814318806f87ba
jsoh07 <85730620+jsoh07@users.noreply.github.com>
2024-10-24 16:32:43 +0900

    Update 문자열 내 p와 y의 개수.js

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,9 +1,20 @@
 function solution(s){
+    // 이렇게 소문자로 통일해서 해도 되고
     s = s.toLowerCase();
 
     // 정규표현식을 사용해 갯수 파악
     const pCount = (s.match(/p/g) || []).length;
     const yCount = (s.match(/y/g) || []).length;
+
+    return pCount === yCount;
+}
+
+function solution(s){
+
+    // 대,소문자 구분없이
+    // 정규표현식을 사용해 갯수 파악,
+    const pCount = (s.match(/p/gi) || []).length;
+    const yCount = (s.match(/y/gi) || []).length;
 
     return pCount === yCount;
 }
