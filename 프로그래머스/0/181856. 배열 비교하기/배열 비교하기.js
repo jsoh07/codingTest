77ca9b3ccd8b1a8c53a1e4dd4834fb0a556c6803
jsoh07 <85730620+jsoh07@users.noreply.github.com>
2024-11-16 15:32:56 +0900

    [level 0] Title: 배열 비교하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -0,0 +1,12 @@
+function solution(arr1, arr2) {
+    const arr1Sum = arr1.reduce((acc, curr) => acc + curr);
+    const arr2Sum = arr2.reduce((acc, curr) => acc + curr);
+    
+    if(arr1.length < arr2.length) return -1;
+    else if(arr1.length > arr2.length) return 1;
+    
+    if(arr1Sum < arr2Sum) return -1;
+    else if(arr1Sum > arr2Sum) return 1;
+    
+    return 0;
+}   
