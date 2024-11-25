b47d090f2b107b87451b3a32d7e35d2bd3338ce7
jsoh07 <85730620+jsoh07@users.noreply.github.com>
2024-11-25 21:16:15 +0900

    Update 두 수의 연산값 비교하기.js

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -1,10 +1,7 @@
-function solution(a, b) {
-    const sA = String(a);
-    const sB = String(b);
-    const sumS = sA + sB;
+function solution(a, b) { 
+    const sumS = String(a) + String(b);
     const mul = 2 * a * b;
     if(sumS > mul) return Number(sumS);
     else if(sumS < mul) return mul;
     else return Number(sumS);
-
 }
